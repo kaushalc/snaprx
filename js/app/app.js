028f5066585a5ef5d70c2a3ef7238196d54bed84
Kaushal Cavale <kaushal@deck.in>
2014-06-21 21:41:22 +0530

    Added Login view with Login.html

--- js/app/app.js
+++ js/app/app.js
@@ -7,10 +7,17 @@ angular.module('snaprx', ['ui.router'])
               templateUrl: 'html/Home.html',
               controller: 'SnapRxController'
             };
+            var login = {
+              name: 'home.login',
+              url:'/login',
+              templateUrl: 'html/Login.html',
+              controller: 'SnapRxController'
+            };
              $stateProvider.state(home);
+             $stateProvider.state(login);
 	   }])
 	   .run(['$state', function ($state) {      
-           $state.transitionTo('home'); 
+           $state.transitionTo('home.login'); 
         }])
 	   .controller('SnapRxController',['$scope','$state','$http',function($scope,$state,$http){
               	
