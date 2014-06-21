59180e27f2048c0a0590ee9d5b589fce7f993993
Kaushal Cavale <kaushal@deck.in>
2014-06-21 20:29:54 +0530

    Added ng-app

---
+++ js/app/app.js
@@ -0,0 +1,18 @@
+var SnapRxApp = 
+angular.module('snaprx', ['ui.router'])
+	   .config(['$stateProvider', function($stateProvider){
+	   	var home = {
+              name: 'home',
+              url:'/home',
+              templateUrl: '/html/Home.html',
+              controller: 'SnapRxController'
+            };
+             $stateProvider.state(home);
+	   }])
+	   .run(['$state', function ($state) {      
+           $state.transitionTo('home'); 
+        }])
+	   .controller('SnapRxController',['$scope','$state','$http',function($scope,$state,$http){
+              	
+        }])
+;
